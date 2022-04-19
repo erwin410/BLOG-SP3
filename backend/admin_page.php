dce88e3fb3f9c3c9639bb629189c6ac1a0340d59
lvsprod <erwinpateux@gmail.com>
2022-04-19 19:38:26 +0400

    Ajout Du header au Backend

--- backend/admin_page.php
+++ backend/admin_page.php
@@ -16,19 +16,23 @@ if(!isset($_SESSION['admin_name'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=""> 
-    <title>Admin Page</title>
+    <link rel="stylesheet" href="../styling/home.css">
+    <title>Liste des Poste</title>
 </head>
 <body>
-    <div class="container">
-        <div class="content">
-            <h3>hi, <span>admin</span></h3>
-            <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
-            <p>this is an admin page</p>
-            <a href="login_form.php" class="btn">login</a>
-            <a href="register_form.php" class="btn">register</a>
-            <a href="logout.php" class="btn">logout</a>
-        </div>
-    </div>
+        <header>
+            <div class="container header">
+                <a href="../index.php" class="logo">Marg.<span>Lab</span></a>
+                <div class="menutoggle">
+                    <i class="fas fa-bars"></i>
+                </div>
+
+                <ul class="navigation">
+                    <li><a href="../index.php">Accueil</a></li>
+                    <li><a href="register_form.php" class="btn">register</a></li>
+                    <li><a href="logout.php" class="btn">logout</a></li>
+                </ul>
+            </div>
+        </header>
 </body>
 </html>
--- backend/register_form.php
+++ backend/register_form.php
@@ -37,9 +37,24 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styling/register.css">
+    <link rel="stylesheet" href="../styling/home.css">
     <title>register form</title>
 </head>
 <body>
+        <header>
+            <div class="container header">
+                <a href="../index.php" class="logo">Marg.<span>Lab</span></a>
+                <div class="menutoggle">
+                    <i class="fas fa-bars"></i>
+                </div>
+
+                <ul class="navigation">
+                    <li><a href="../index.php">Accueil</a></li>
+                    
+                </ul>
+            </div>
+        </header>
+
     <div class="form-container">
         <form action="" method="post">
             <h3>register now</h3>
